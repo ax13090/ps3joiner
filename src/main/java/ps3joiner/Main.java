c3d9aad48fc447ec074f7b7b27811a71a91919a2
Axel de Sablet <contact@axel-desablet.fr>
2014-01-04 18:58:07 +0100

    Add some command-line parsing code

--- src/main/java/ps3joiner/Main.java
+++ src/main/java/ps3joiner/Main.java
@@ -7,6 +7,28 @@
 
 package ps3joiner;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 	
+	public static void main(final String[] args) throws ParseException {
+		final CommandLine line = parseCommandLine(args);
+		
+		final boolean dryRun = line.hasOption("-n");
+		
+
+		LoggerFactory.getLogger(Main.class).trace("Dry-run: " + dryRun);
+	}
+
+	public static CommandLine parseCommandLine(final String[] args) throws ParseException {
+		final Options cliOptions = new Options();		
+		cliOptions.addOption("n", false, "dry-run");
+		
+		final CommandLine line = new BasicParser().parse(cliOptions, args);
+		return line;
+	}
 }
